c8e68153829c3e6b6a56ad4287dac38e51fbab51
Samuel Tan <samuel.tan@monash.edu>
2016-10-17 23:26:31 +1100

    created sbp-analysis.Rmd, more or less finished it too

--- scripts/load.r
+++ scripts/load.r
@@ -1,7 +1,1 @@
 
-require(data.table)
-require(magrittr)
-require(tidyr)
-
-sbp.ccsdt <- read.csv(file = "~/GoogleDrive/srs-app/data/sbp-ccsdt-adz.edat", header = TRUE
-         , sep = "|", strip.white = TRUE) %>% data.table()
